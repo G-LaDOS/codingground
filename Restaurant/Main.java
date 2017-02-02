8f9e9f4886da1a061e399a03164f0233f54a9310
Maximus2194 <jromeromaximus@gmail.com>
2017-02-02 08:56:05 -0800

    Update Main.java

--- Restaurant/Main.java
+++ Restaurant/Main.java
@@ -2,5 +2,17 @@ public class HelloWorld{
 
      public static void main(String []args){
         System.out.println("Hello World");
+        Waiter John = new Waiter();
+        John.setOrder("Pink Pancakes");
+        John.setServeNumber(1);
+        John.setIsPaid(true);
+        John.orderUp();
+        
+        Waiter Mike = new Waiter();
+        Mike.setOrder("All the food in the kitchen");
+        Mike.setServeNumber(2);
+        Mike.setIsPaid(false);
+        Mike.orderUp();
+        
      }
 }

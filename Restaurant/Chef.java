7e5ba9711feeecc2af55931bcf53292e1bb09bd2
Maximus2194 <jromeromaximus@gmail.com>
2017-02-03 12:13:35 -0800

    Update Chef.java

--- Restaurant/Chef.java
+++ Restaurant/Chef.java
@@ -1,11 +1,21 @@
 public class Chef{
     private String name;
+    private String order;
     
-    Chef(String Id){
+    Chef(String Id, String order){
         name = Id;
+        this.order = order;
+        
     }
     
+
     public void greet(){
         System.out.println("Hello I am " + name + ".");
     }
+    
+    public void cook(){
+        System.out.println("I will begin making " + order + ".");
+    }
+    
+    
 }

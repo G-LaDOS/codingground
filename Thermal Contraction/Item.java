6e9574778f40e3c3e6e1ff5aa298e625c0ad2a66
Maximus2194 <jromeromaximus@gmail.com>
2017-02-06 14:47:50 -0800

    Create Item.java

---
+++ Thermal Contraction/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    private String StackSize;
+    private int Health;
+
+    public Item(){
+    
+    }
+    
+    public void setStackSize(String StackSize){
+        this.StackSize = StackSize;
+        
+    }
+    
+    public void setHealth(int Health){
+        this.Health = Health;
+    }
+}

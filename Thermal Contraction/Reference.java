08ac9a99cfe15dad3e05dcaf85774cdaf9584776
Maximus2194 <jromeromaximus@gmail.com>
2017-02-06 08:47:33 -0800

    Create Reference.java

---
+++ Thermal Contraction/Reference.java
@@ -0,0 +1,8 @@
+public class Reference{
+    
+    public final String MODID = "TC";
+    public final String NAME = "Thermal Contraction";
+    public final String VERSION = "1.0";
+    public final String MINECRAFTVERSION = "1.10.2";
+    
+}

ad8208fc4fd200d6e3f0ec0bbd33c1547708569b
Maximus2194 <jromeromaximus@gmail.com>
2017-02-06 08:52:17 -0800

    Update Reference.java

--- Thermal Contraction/Reference.java
+++ Thermal Contraction/Reference.java
@@ -5,4 +5,8 @@ public class Reference{
     public final String VERSION = "1.0";
     public final String MINECRAFTVERSION = "1.10.2";
     
+    public void init(){
+        System.out.println(MODID + "\n Name:" + NAME + "\n Version: " + VERSION + "\n Minecraft Version: " + MINECRAFTVERSION + "\n");
+    }
+    
 }

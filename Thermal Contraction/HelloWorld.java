cedf33a4bac24913e39f460a13caa97a43d1d9c1
Maximus2194 <jromeromaximus@gmail.com>
2017-02-07 12:14:02 -0800

    Update HelloWorld.java

--- Thermal Contraction/HelloWorld.java
+++ Thermal Contraction/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld{
      public static void main(String []args){
          Reference Init = new Reference();
          Init.init();
+         Item Ball = new Item("Ball");
+         Item Fly = new Item("Fly");
         
      }
 }

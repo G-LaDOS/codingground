aa542a21094d2a35ef8c4dd8678535c2f49f36bf
Maximus2194 <jromeromaximus@gmail.com>
2017-02-07 12:13:13 -0800

    Update Registery.java

--- Thermal Contraction/Registery.java
+++ Thermal Contraction/Registery.java
@@ -1,9 +1,9 @@
 public class Registery {
     private String name;
     
-    public void registerItem(String unlocalizedName){
-       unlocalizedName = name;
-       System.out.println(name + " is registered.");
+    public Registery(String name){
+       this.name = name;
+       System.out.println(name + " is registered.\n");
     }
     
     
